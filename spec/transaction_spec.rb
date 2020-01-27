ee4d6e12ee82c307e7a7a364655db6ff28ff0423
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-27 22:31:54 +0000

    test: add type of transaction

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,9 +2,14 @@ require 'Transaction'
 
 describe Transaction do
 
-  let(:subject) { described_class.new(1000) }
+  let(:subject) { described_class.new(1000, "CREDIT") }
 
   it "should have an amount" do
     expect(subject.amount).to eq 1000
   end
+
+  it "should have a type" do
+    expect(subject.type).to eq "CREDIT"
+  end
+
 end
