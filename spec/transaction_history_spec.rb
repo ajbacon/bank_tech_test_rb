4d1c534ff134c3da3bcbeebeb659271564a19ffe
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-27 15:13:24 +0000

    feat: most recent transactions at beginning of array. test: above to pass

--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -22,12 +22,17 @@ describe TransactionHistory do
     end
 
     it "should store multiple transactions to the transaction history" do 
-      date = Time.now.strftime("%d/%m/%Y")
-
       subject.add_transaction(1000, 0)
       subject.add_transaction(-500, 1000)
 
       expect(subject.transactions.length).to eq 2
+    end
+
+    it "should store most recent transaction at the beginning of the array" do 
+      subject.add_transaction(1000, 0)
+      subject.add_transaction(-500, 1000)
+
+      expect(subject.transactions[0][:debit]).to eq 500
     end
   end
 end
