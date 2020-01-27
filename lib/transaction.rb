c76b09ffdacc3bf964fa5281cfed138bdd91324e
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-27 22:43:32 +0000

    test: add tests for date and total balance

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,18 @@
 class Transaction
 
-  attr_reader :amount, :type
+  attr_reader :amount, :type, :date, :balance
   
-  def initialize(amount, type)
+  def initialize(amount, type, date = Time.now.strftime("%d/%m/%Y"), balance)
     @amount = amount
     @type = type
+    @date = date
+    @balance = new_balance(balance)
+  end
+
+  private
+
+  def new_balance(balance)
+    @type == "CREDIT" ? balance + @amount : balance - @amount
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,7 +2,7 @@ require 'Transaction'
 
 describe Transaction do
 
-  let(:subject) { described_class.new(1000, "CREDIT") }
+  let(:subject) { described_class.new(1000, "CREDIT", "27/01/2020", 500) }
 
   it "should have an amount" do
     expect(subject.amount).to eq 1000
@@ -12,4 +12,11 @@ describe Transaction do
     expect(subject.type).to eq "CREDIT"
   end
 
+  it "should have a date attached" do
+    expect(subject.date).to eq "27/01/2020"
+  end
+
+  it "should update the total balance" do
+    expect(subject.balance).to eq 1500
+  end
 end
