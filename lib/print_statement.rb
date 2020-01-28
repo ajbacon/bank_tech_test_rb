51f21d06a53b104089bdfd1786643b885e5fbe6b
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-28 11:12:59 +0000

    feat: add print transactions

--- lib/print_statement.rb
+++ lib/print_statement.rb
@@ -1,10 +1,18 @@
 class PrintStatement
 
-
+  def initialize(transaction_history)
+    @transaction_history = transaction_history
+  end
 
   def print_header
     puts "date || credit || debit || balance"
   end
 
-
+  def print_transactions
+    @transaction_history.transactions.each do |transaction|
+      amount_str = "|| #{'%.2f' % transaction.amount} ||  ||" if transaction.type == "CREDIT" 
+      amount_str = "||  || #{'%.2f' % transaction.amount} ||" if transaction.type == "DEBIT" 
+      puts "#{transaction.date} #{amount_str} #{'%.2f' % transaction.balance}"
+    end
+  end
 end
--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -1,18 +1,21 @@
 require 'print_statement'
 
 describe PrintStatement do
+  let(:transaction_history) { double :transaction_history }
+  let(:transaction1) { double :transaction, amount: 1000.00, type: "CREDIT", date: "10/01/2012", balance: 1000.00 }
+  let(:transaction2) { double :transaction, amount: 500.00, type: "DEBIT", date: "14/01/2012", balance: 500.00 }
+  subject { described_class.new(transaction_history) }
 
   describe "#print_header" do
     it "should print the header to stdout" do 
-      expect { subject.header }.to output("date || credit || debit || balance\n").to_stdout
+      expect { subject.print_header }.to output("date || credit || debit || balance\n").to_stdout
     end
   end
 
   describe "#print_transactions" do
-  it "should print the header to stdout" do 
-    expect { subject.header }.to output("date || credit || debit || balance\n").to_stdout
+    it "should print the transactions to stdout, given an array of transactions" do 
+      allow(transaction_history).to receive(:transactions) { [transaction2, transaction1] }
+      expect { subject.print_transactions }.to output("14/01/2012 ||  || 500.00 || 500.00\n10/01/2012 || 1000.00 ||  || 1000.00\n").to_stdout
+    end
   end
-end
-
-
 end
