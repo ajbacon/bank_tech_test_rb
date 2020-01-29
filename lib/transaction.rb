a162ec69ce52e53f842eb52890d9535a1872004e
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-29 09:12:47 +0000

    refactor: transaction class to update balance based on changes to bank_account

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,11 +1,17 @@
 class Transaction
 
-  attr_reader :amount, :type, :date, :balance
+  attr_reader :amount, :type, :balance, :date
   
   def initialize(amount, type, balance, date = Time.now.strftime("%d/%m/%Y"))
     @amount = amount
     @type = type
-    @balance = balance
+    @balance = update_balance(amount, balance, type)
     @date = date
+  end
+
+  private
+
+  def update_balance(amount, balance, type)
+    type == "CREDIT" ? balance + amount : balance - amount
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -16,7 +16,17 @@ describe Transaction do
     expect(subject.date).to eq "27/01/2020"
   end
 
-  it "should update the total balance" do
-    expect(subject.balance).to eq 1000
+
+  describe "#balance" do
+    let(:credit) { described_class.new(1000, "CREDIT", 1000, "27/01/2020") }
+    let(:debit) { described_class.new(500, "DEBIT", 1000, "27/01/2020") }
+
+    it "should update the balance by adding the amount if the transaction type is 'CREDIT" do
+      expect(credit.balance).to eq 2000
+    end
+
+    it "should update the balance by adding the amount if the transaction type is 'DEBIT" do
+      expect(debit.balance).to eq 500
+    end
   end
 end
